a96157eb9a860700dec66ad0ea8eb83564afa68e
brtlvrs <bart@bartlievers.nl>
2021-04-14 11:32:50 +0200

    added

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,9 @@
 
 ## create pwcli wrapper to run the pwcli container interactively
 
-cat > /usr/local/bin/pwcli << EOF
+rm -f /usr/local/bin/pwcli
+
+cat > /usr/local/bin/pwcli << 'EOF'
 #!/usr/bin/env bash
 #
 #  A script wrapper for running the docker image pwcli interactively
@@ -14,7 +16,7 @@ docker run --rm -it \
   -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
   -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
   -v $(pwd):/pwsh \
- $docker_img pwsh "$@"
+  $docker_img pwsh "$@"
 EOF
 
 chmod +x /usr/local/bin/pwcli
